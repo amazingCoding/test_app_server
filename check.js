4030cd734ef8309609bbe95410a70cf1008bf05b
mokeyCode <sanylanv@gmail.com>
2024-06-18 15:44:39 +0800

    fix

--- check.js
+++ check.js
@@ -1,6 +1,7 @@
 const { PublicKey } = require('@solana/web3.js');
 const { default: axios } = require('axios');
 const bitcoin = require('bitcoinjs-lib'); // 引入 bitcoinjs-lib 库
+const { formatUnits } = require('ethers');
 // 校验 Base58 地址
 function validateBase58Address(address) {
   try {
@@ -10,7 +11,6 @@ function validateBase58Address(address) {
     return false;
   }
 }
-
 // 校验 Bech32 地址
 function validateBech32Address(address) {
   try {
@@ -20,7 +20,6 @@ function validateBech32Address(address) {
     return false;
   }
 }
-
 // 校验比特币地址
 function validateBTCAddress(address) {
   // 判断地址格式
@@ -30,41 +29,6 @@ function validateBTCAddress(address) {
     return validateBech32Address(address);
   } else {
     return false;
-  }
-}
-function calculateBalance(data) {
-  // const chainReceived = data.chain_stats.funded_txo_sum;
-  // const chainSpent = data.chain_stats.spent_txo_sum;
-  // const mempoolReceived = data.mempool_stats.funded_txo_sum;
-  // const mempoolSpent = data.mempool_stats.spent_txo_sum;
-
-  // const totalReceived = chainReceived + mempoolReceived;
-  // const totalSpent = chainSpent + mempoolSpent;
-
-  // const balance = totalReceived - totalSpent;
-  // return balance;
-  const chainReceived = data.chain_stats.funded_txo_sum;
-  const chainSpent = data.chain_stats.spent_txo_sum;
-  const mempoolReceived = data.mempool_stats.funded_txo_sum;
-  const mempoolSpent = data.mempool_stats.spent_txo_sum;
-
-  const totalReceived = chainReceived + mempoolReceived;
-  const totalSpent = chainSpent + mempoolSpent;
-
-  const balanceSatoshis = totalReceived - totalSpent;
-  const balanceBTC = balanceSatoshis / 100000000;
-  return {
-    balanceSatoshis,
-    balanceBTC
-  }
-}
-const getCoinRate = async (coin) => {
-  if (coin === 'UNKNOWN') return -1
-  try {
-    const rateRes = await axios.get(`https://min-api.cryptocompare.com/data/price?fsym=${coin}&tsyms=USD`)
-    return rateRes.data.USD
-  } catch (error) {
-    return -1
   }
 }
 const checkIsETHAddress = (address) => {
@@ -83,6 +47,32 @@ function isValidSolanaAddress(address) {
     return false;
   }
 }
+function calculateBalance(data) {
+  const chainReceived = data.chain_stats.funded_txo_sum;
+  const chainSpent = data.chain_stats.spent_txo_sum;
+  const mempoolReceived = data.mempool_stats.funded_txo_sum;
+  const mempoolSpent = data.mempool_stats.spent_txo_sum;
+
+  const totalReceived = chainReceived + mempoolReceived;
+  const totalSpent = chainSpent + mempoolSpent;
+
+  const balanceSatoshis = totalReceived - totalSpent;
+  const balanceBTC = formatUnits(balanceSatoshis, 8);
+  return {
+    balanceSatoshis,
+    balanceBTC
+  }
+}
+const getCoinRate = async (coin) => {
+  if (coin === 'UNKNOWN') return -1
+  try {
+    const rateRes = await axios.get(`https://min-api.cryptocompare.com/data/price?fsym=${coin}&tsyms=USD`)
+    return rateRes.data.USD
+  } catch (error) {
+    return -1
+  }
+}
+
 module.exports = {
   validateBTCAddress,
   calculateBalance,
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,32 @@
-var express = require('express');
-var { validateBTCAddress, calculateBalance, getCoinRate, checkIsETHAddress, checkIsTronAddress, isValidSolanaAddress } = require('../check');
-const { default: axios } = require('axios');
-const { JsonRpcProvider, formatEther, formatUnits } = require('ethers');
-const { Contract } = require('ethers');
-const TronWeb = require('tronweb');
-const solanaWeb3 = require('@solana/web3.js');
-const splToken = require("@solana/spl-token")
-// import { Client } from '@solflare-wallet/utl-sdk';
-const { Client } = require('@solflare-wallet/utl-sdk');
-var router = express.Router();
+const express = require('express')
+const { validateBTCAddress, calculateBalance, getCoinRate, checkIsETHAddress, checkIsTronAddress, isValidSolanaAddress } = require('../check')
+const { default: axios } = require('axios')
+const { JsonRpcProvider, formatEther, formatUnits } = require('ethers')
+const { Contract } = require('ethers')
+const TronWeb = require('tronweb')
+const solanaWeb3 = require('@solana/web3.js')
+const { Client } = require('@solflare-wallet/utl-sdk')
+const router = express.Router()
 
+const getError = (res, error) => {
+  console.log(typeof error, error);
+  let message = 'unknown error'
+  if (typeof error === 'string') message = error
+  else if (error.response) message = error.response.data.message
+  else if (error.message) message = error.message
+  res.json({ code: 400, message })
+}
+const getUSD = async (coin, balance) => {
+  const rate = await getCoinRate(coin)
+  let usd = 0
+  if (rate > 0) {
+    usd = rate * Number(balance)
+  }
+  return {
+    usd,
+    rate
+  }
+}
 router.get('/api/v1/balance', async (req, res, next) => {
   const { chainType, address, isTest, symbol, rpcURL, tokenAddress } = req.query
   try {
@@ -17,63 +34,47 @@ router.get('/api/v1/balance', async (req, res, next) => {
       case "BTC": {
         let isBTCAddress = validateBTCAddress(address)
         if (!isBTCAddress) {
-          res.json({ code: 400, message: 'Invalid BTC address' })
+          getError(res, 'Invalid BTC address')
           return
         }
-        try {
-          const response = await axios.get(`https://blockstream.info/api/address/${address}`)
-          const rate = await getCoinRate(chainType)
-          const { balanceSatoshis, balanceBTC } = calculateBalance(response.data)
-          const usd = rate * balanceBTC
-          res.json({ code: 200, data: { balance: balanceSatoshis, balanceFormat: balanceBTC, rate, usd } })
-        } catch (error) {
-          res.json({ code: 400, message: error.message })
-        }
+        const response = await axios.get(`https://blockstream.info/api/address/${address}`)
+        const { balanceSatoshis, balanceBTC } = calculateBalance(response.data)
+        const { usd, rate } = await getUSD('BTC', balanceBTC)
+        res.json({ code: 200, data: { balance: balanceSatoshis, balanceFormat: balanceBTC, rate, usd } })
         return
       }
       case "EVM": {
         if (checkIsETHAddress(address) === false) {
-          res.json({ code: 400, message: 'Invalid ETH address' })
+          getError(res, 'Invalid ETH address')
           return
         }
         if (rpcURL === '') {
-          res.json({ code: 400, message: 'Invalid RPC URL' })
+          getError(res, 'Invalid RPC URL')
           return
         }
         const url = decodeURIComponent(rpcURL)
         const provider = new JsonRpcProvider(url)
         if (!tokenAddress) {
           const balance = await provider.getBalance(address)
-          const rate = await getCoinRate(symbol)
           const balanceFormat = formatEther(balance)
-          const usd = rate * balanceFormat
+          const { usd, rate } = await getUSD('ETH', balanceFormat)
           res.json({ code: 200, data: { balance: balance.toString(), balanceFormat, rate, usd } })
+          return
         }
-        else {
-          // 获取合约余额
-          const erc20Abi = [
-            "function symbol() view returns (string)",
-            "function decimals() view returns (uint8)",
-            "function balanceOf(address owner) view returns (uint256)"
-          ]
-          const contract = new Contract(tokenAddress, erc20Abi, provider)
-          // 先查询 精度
-          const symbol = await contract.symbol();
-          const decimals = await contract.decimals();
-          const balance = await contract.balanceOf(address);
-          console.log(symbol);
-          console.log(decimals);
-          const balanceFormat = formatUnits(balance, decimals)
-          const rate = await getCoinRate(symbol)
-          const usd = rate * balanceFormat
-          res.json({ code: 200, data: { balance: balance.toString(), balanceFormat, rate, usd, symbol } })
-        }
-
+        // 获取合约余额
+        const erc20Abi = ["function symbol() view returns (string)", "function decimals() view returns (uint8)", "function balanceOf(address owner) view returns (uint256)"]
+        const contract = new Contract(tokenAddress, erc20Abi, provider)
+        const symbol = await contract.symbol();
+        const decimals = await contract.decimals();
+        const balance = await contract.balanceOf(address);
+        const balanceFormat = formatUnits(balance, decimals)
+        const { usd, rate } = await getUSD(symbol, balanceFormat)
+        res.json({ code: 200, data: { balance: balance.toString(), balanceFormat, rate, usd, symbol } })
         return
       }
       case "SOL": {
         if (isValidSolanaAddress(address) === false) {
-          res.json({ code: 400, message: 'Invalid SOL address' })
+          getError(res, 'Invalid SOL address')
           return
         }
         const connection = new solanaWeb3.Connection(isTest ? solanaWeb3.clusterApiUrl('devnet') : 'https://sly-hardworking-dew.solana-mainnet.quiknode.pro/f6eb1261fe38b21326f892aabef573a7fa01a80a/')
@@ -82,27 +83,23 @@ router.get('/api/v1/balance', async (req, res, next) => {
           const tokenPublicKey = new solanaWeb3.PublicKey(tokenAddress)
           const utl = new Client();
           const token = await utl.fetchMint(tokenPublicKey)
-          console.log(token);
           const symbol = token?.symbol || 'UNKNOWN'
           let decimals = token?.decimals || 0
           let balance = 0
           const accounts = await connection.getTokenAccountsByOwner(publicKey, { mint: tokenPublicKey })
           for (let account of accounts.value) {
             const tokenAccountInfo = await connection.getParsedAccountInfo(account.pubkey);
-            console.log(tokenAccountInfo.value?.data.parsed);
             balance = tokenAccountInfo.value?.data.parsed.info.tokenAmount.amount;
             if (decimals === 0) decimals = tokenAccountInfo.value?.data.parsed.info.tokenAmount.decimals
           }
           const balanceFormat = decimals === 0 ? 0 : formatUnits(balance, decimals)
-          const rate = await getCoinRate(symbol)
-          const usd = rate === -1 ? 0 : rate * balanceFormat
+          const { usd, rate } = await getUSD(symbol, balanceFormat)
           res.json({ code: 200, data: { balance: balance.toString(), balanceFormat, rate, usd, symbol } })
           return
         }
         const balance = await connection.getBalance(publicKey)
-        const rate = await getCoinRate('SOL')
         const balanceFormat = formatUnits(balance, 9)
-        const usd = rate === -1 ? 0 : rate * balanceFormat
+        const { usd, rate } = await getUSD('SOL', balanceFormat)
         res.json({ code: 200, data: { balance: balance.toString(), balanceFormat, rate, usd } })
 
 
@@ -110,15 +107,13 @@ router.get('/api/v1/balance', async (req, res, next) => {
       }
       case "TRON": {
         if (checkIsTronAddress(address) === false) {
-          res.json({ code: 400, message: 'Invalid TRON address' })
+          getError(res, 'Invalid TRON address')
           return
         }
-        const header = isTest ? {} : { 'TRON-PRO-API-KEY': 'beeb2792-babf-446c-8b2c-a62149b03f6e' }
-        const tronWeb = new TronWeb({
-          fullHost: isTest ? 'https://api.shasta.trongrid.io' : 'https://api.trongrid.io',
-          headers: header
-        });
-        tronWeb.setAddress(address);
+        const headers = isTest ? {} : { 'TRON-PRO-API-KEY': 'beeb2792-babf-446c-8b2c-a62149b03f6e' }
+        const fullHost = isTest ? 'https://api.shasta.trongrid.io' : 'https://api.trongrid.io'
+        const tronWeb = new TronWeb({ fullHost, headers })
+        tronWeb.setAddress(address)
         if (tokenAddress && tokenAddress !== '') {
           const abi = [
             {
@@ -173,39 +168,21 @@ router.get('/api/v1/balance', async (req, res, next) => {
           const symbol = await contract.symbol().call()
           const decimals = await contract.decimals().call()
           const balance = await contract.balanceOf(address).call()
-          console.log(balance);
-
           const balanceFormat = formatUnits(balance.toString(), decimals)
-          const rate = await getCoinRate(symbol)
-          const usd = rate * balanceFormat
+          const { usd, rate } = await getUSD(symbol, balanceFormat)
           res.json({ code: 200, data: { balance: balance.toString(), balanceFormat, rate, usd, symbol } })
           return
         }
         const response = await tronWeb.trx.getBalance(address)
         const balance = response
         const balanceFormat = formatUnits(balance, 6)
-        const rate = await getCoinRate('TRX')
-        const usd = rate * balanceFormat
+        const rate = await getCoinRate('TRX', balanceFormat)
         res.json({ code: 200, data: { balance: balance.toString(), balanceFormat, rate, usd } })
         return
       }
     }
   } catch (error) {
-    console.log(typeof error, error);
-    let message = 'unknown error'
-    if (typeof error === 'string') {
-      message = error
-    }
-    else if (error.response) {
-      message = error.response.data.message
-    }
-    else if (error.message) {
-      message = error.message
-    }
-    else {
-      message = error.toString()
-    }
-    res.json({ code: 400, message })
+    getError(res, error)
     return
   }
   res.json({
@@ -216,5 +193,4 @@ router.get('/api/v1/balance', async (req, res, next) => {
   })
 
 })
-
 module.exports = router;
